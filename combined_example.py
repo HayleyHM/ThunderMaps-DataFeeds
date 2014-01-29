a124b98a2c0d51bfff6fe9a5430a4e0956a71093
Hayley Hume-Merry <hayley@thundermaps.com>
2014-01-29 15:24:13 +1300

    Added photo upload

--- combined_example.py
+++ combined_example.py
@@ -4,55 +4,74 @@
 # while caching which data has already been posted to ThunderMaps.
 # It should be used for a data feed that doesn't provide the ability to specifiy the start date for the data returned.
 #
-#Author: Daniel Gibbs <danielgibbs.name> and Hayley Hume-Merry <hayley@thundermaps.com>
+#Author: Daniel Gibbs <danielgibbs.name>
 #
-import urllib.request
+import requests
 import pytz, datetime
 import time
-import xml.etree.ElementTree as ET
 import sys
-sys.path.append(r'C:\Users\H\Documents\Jobs\ThunderMaps\Data Feeds\ThunderMaps')#r'/usr/local/thundermaps')
-import Sthundermaps
-import html.parser
+sys.path.append(r'/usr/local/thundermaps') #Grabs the Pthundermaps.py location for import
+import Pthundermaps #thundermaps.py file with photo upload capabilities
+import json
 
-key = '<your_api_key>'
-account_id = '<thundermaps_account>'
+key = '<THUNDERMAPS_API_KEY>'
+account_id = '<THUNDERMAPS_ACCOUNT_ID>'
 
-class Dispatch:
+class Incidents:
 	def format_feed(self):
-		h = html.parser.HTMLParser()
-		dispatch_file = urllib.request.urlretrieve('http://www.dot.nd.gov/travel-info-v2/georss/roads.xml', 'roads.xml')
-		tree = ET.parse('roads.xml')
-		listings = []
-		for entry in tree.iter('{http://www.w3.org/2005/Atom}entry'):
-				try: 
-						location = entry.find('{http://www.georss.org/georss}point').text.split()
-				except:
-						location = entry.find('{http://www.georss.org/georss}line').text.split()
-				condition_id = entry.find('{http://www.w3.org/2005/Atom}id').text
-				date = entry.find('{http://www.w3.org/2005/Atom}updated').text.replace('T', ' ')
-				summary = entry.find('{http://www.w3.org/2005/Atom}summary').text.split()
-				description = ''
-				for i in summary:
-						i = i.strip().replace('<strong>', '').replace('</strong>', '')
-						description += i + ' '
-				road_type = summary[0].strip()
-				#format each parameter into a dictionary
-				listing = {"occurred_on":date, 
-		                           "latitude":location[0], 
-		                           "longitude":location[1], 
-		                           "description":description,  
-		                           "category_name":'North Dakota Road Conditions - ' + road_type.title(),
-		                           "source_id":condition_id}
-				#create a list of dictionaries
+			listings = []
+			photo_ids = self.get_photo_ids()
+			# Uses the photo id's to grab the photo information from the GeoStock Photo's API
+			for key,value in photo_ids.items():
+				photo_params = {"id":key}
+				photo_feed = requests.get('http://geostockphoto.com/photo/getInfo/apiKey/kRqJ2NgO', params=photo_params)
+				try:
+					info = photo_feed.json()
+				except ValueError:
+					print('continue')
+					continue
+				date = info['approvedDate']
+				format_date = self.format_datetime(date)
+				rank_id = info['rate']
+				rank_id = "GeoStock Photo" + (" - Rank %s" %rank_id)
+				listing = {"occurred_on":format_date, 
+				           "latitude":info['lat'], 
+				           "longitude":info['lng'], 
+				           "description":str(info['title'].title()) + '<br/>' + 'User: ' + str(info['idUser']),
+				           "category_name":rank_id,
+				           "source_id":key,
+				           "attachment_url":value
+				        }
 				listings.append(listing)
-		print(listings)
-		return listings				
+			return listings
+
+	def get_photo_ids(self):
+		# Uses the GeoStock Photos API to grab photo id's and thumbnail images
+		search_params = {"thumb":"430"}
+		search_feed = requests.get('http://geostockphoto.com/photo/getSearch/apiKey/kRqJ2NgO', params=search_params)
+		all_photos = search_feed.json()
+		for photo in all_photos["photo"]:
+			# Stores the photo id's and images in a dict
+			photo_ids = {photo["id"]:photo["src"]}
+		return photo_ids
+	
+	def format_datetime(self, date):
+		#convert date and time format from GMT to UTC
+		date_time = date.replace('/', ' ').split()
+		monthDict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+		if date_time[1] in monthDict:
+			month = monthDict[date_time[1]]
+		date_time = str(date_time[2]) + '-' + str(month) + '-' + str(date_time[0]) + ' ' + str(date_time[4])
+		local = pytz.timezone("GMT")
+		naive = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M")
+		local_dt = local.localize(naive, is_dst = None)
+		utc_dt = str(local_dt.astimezone(pytz.utc))
+		return utc_dt	
 	
 class Updater:
 	def __init__(self, key, account_id):
-		self.tm_obj = Sthundermaps.ThunderMaps(key)
-		self.feed_obj = Dispatch()
+		self.tm_obj = Pthundermaps.ThunderMaps(key)
+		self.feed_obj = Incidents()
 		self.account_id = account_id
 		
 	def start(self):
@@ -84,8 +103,15 @@ class Updater:
 			if len(reports) > 0:
 				# Upload 10 at a time.
 				for some_reports in [reports[i:i+10] for i in range(0, len(reports), 10)]:
+					for report in some_reports:
+						# Add image
+						image_id = self.tm_obj.uploadImage(report["attachment_url"])
+						if image_id != None:
+							print("[%s] Uploaded image for listing %s..." % (time.strftime("%c"), report["source_id"]))
+							report["attachment_ids"] = [image_id]
+						del report["attachment_url"]
 					print("Sending %d reports..." % len(some_reports))
-					self.tm_obj.sendReports(self.account_id, some_reports)
+					self.tm_obj.sendReports(account_id, some_reports)
 					time.sleep(3)
 			# Save the posted source_ids.
 			try:
